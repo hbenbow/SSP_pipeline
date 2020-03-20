d797bc60ca833f4257e8a93438eab4eaf0bd3a65
Harriet Benbow <hbenbow@borlaug.ucd.ie>
2020-03-20 15:50:48 +0000

    improved the comment line

--- ssp.sh
+++ ssp.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-#this surveys length opf proteins and detects signal peptides
+#this surveys length of proteins and detects signal peptides
+#that output is then read by the R script which concatenates the results
 FILES=$1/*
 mkdir $3
 for file in $FILES
